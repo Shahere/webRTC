daf128f8f851dff0eca236c82c4889db8f4ee0c8
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-11-21 11:43:23 +0100

    api

---
+++ api/src/HelloWorld.ts
@@ -0,0 +1,5 @@
+export class HelloWorld {
+  printHello(): void {
+    console.log("Hello from my-ts-package!");
+  }
+}
---
+++ api/webpack.config.cjs
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    library: {
+      type: "module",
+    },
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
