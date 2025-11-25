f5526a20feb97581c4d945e92dd58e8fec6e148b
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-11-25 13:16:06 +0100

    start w

---
+++ api/src/Stream.ts
@@ -0,0 +1,29 @@
+class Stream {
+  mediastream: MediaStream;
+  domElement: undefined | HTMLVideoElement;
+
+  constructor(mediastream: MediaStream) {
+    this.mediastream = mediastream;
+  }
+
+  static async getCamera(video: boolean, audio: boolean): Promise<Stream> {
+    let mediastream = await navigator.mediaDevices.getUserMedia({
+      video: video,
+      audio: audio,
+    });
+    let newStream = new Stream(mediastream);
+    return newStream;
+  }
+  static getScreen() {}
+
+  attachToElement(domElement: HTMLVideoElement): void {
+    domElement.srcObject = this.mediastream;
+  }
+
+  detachToElement() {
+    if (!this.domElement) return;
+    this.domElement.srcObject = null;
+  }
+}
+
+export { Stream };
--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,10 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Stream } from "meetmesavinien";
+import { useRef } from "react";
 
 function App() {
+  let localStreamRef = useRef();
+  async function startLocalStream() {
+    let localStream = await Stream.getCamera(true, true);
+    localStream.attachToElement(localStreamRef.current);
+  }
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="content">
+        <button id="startstream" onClick={startLocalStream}>
+          Start stream
+        </button>
+        <video autoPlay ref={localStreamRef}></video>
+      </div>
     </div>
   );
 }
