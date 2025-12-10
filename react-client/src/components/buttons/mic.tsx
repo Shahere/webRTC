a898a3ee0c721e028ac1d1c14e85083a03a2b8c5
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-12-10 18:18:13 +0100

    audio

--- react-client/src/components/buttons/mic.tsx
+++ react-client/src/components/buttons/mic.tsx
@@ -1,6 +1,20 @@
 import { Mic, MicOff } from "lucide-react";
+import { iConferenceContext, ConferenceContext } from "../../App";
+import { useContext } from "react";
 
-export default function MicToggleButton({ muted = false }) {
+export interface AudioButtonParams {
+  muted: boolean;
+  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+}
+
+export default function MicToggleButton({ muted, onClick }: AudioButtonParams) {
+  const { stream, setStream }: iConferenceContext =
+    useContext(ConferenceContext);
+
+  function disableAudio(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    stream?.muteAudio();
+    onClick(e);
+  }
   return (
     <button
       className={`
@@ -13,6 +27,9 @@ export default function MicToggleButton({ muted = false }) {
         }
         text-white
       `}
+      onClick={(e) => {
+        disableAudio(e);
+      }}
     >
       {muted ? <MicOff size={40} /> : <Mic size={40} />}
     </button>
