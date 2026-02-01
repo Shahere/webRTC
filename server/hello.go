5f94c2bdbe334bddd27954426c0dad28d737b53f
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2026-02-01 18:33:09 +0100

    go

---
+++ server/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": "Hello world",
+	})
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+
+	http.ListenAndServe(":3000", nil)
+}
