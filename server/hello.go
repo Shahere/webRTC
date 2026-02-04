de19f62ff83c970aff6e9b26dd95e97c2c977f2d
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2026-02-04 20:31:20 +0100

    vsy go

--- server/hello.go
+++ server/hello.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	socketio "github.com/zishang520/socket.io/servers/socket/v3"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +19,23 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 
 	http.ListenAndServe(":3000", nil)
+
+	io := socketio.NewServer(nil, nil)
+	io.On("connection", func(clients ...any) {
+		socket := clients[0].(*socketio.Socket)
+
+		log.Println("client connecté :", socket.Id())
+
+		socket.On("ping", func(msg ...any) {
+			log.Println("ping reçu :", msg)
+			socket.Emit("pong", "hello depuis Go ")
+		})
+
+		socket.On("disconnect", func(reason ...any) {
+			log.Println("client déconnecté :", reason)
+		})
+	})
+
+	log.Println("GO run")
+
 }
