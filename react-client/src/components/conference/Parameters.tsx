d562efe83d22c44c857b25262d29f27569502bcd
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2026-03-06 20:52:25 +0100

    hide params

--- react-client/src/components/conference/Parameters.tsx
+++ react-client/src/components/conference/Parameters.tsx
@@ -26,7 +26,10 @@ export function Parameters({ openParameters }: iConferenceParameters) {
   }
 
   return (
-    <div className="z-[99999] h-screen w-[30%] absolute right-0 top-0 bg-gray-800 flex justify-center">
+    <div
+      className={`z-[99999] h-screen w-[30%] absolute ${openParameters ? "right-0" : "right-[-30%]"} top-0 bg-gray-800 flex justify-center`}
+    >
+      //TODO Add transition
       <div className="mt-[10%] p-[7%]">
         <h1 className="text-4xl font-bold text-heading mb-[7%]">Parameters</h1>
         <div className="flex flex-col gap-5">
