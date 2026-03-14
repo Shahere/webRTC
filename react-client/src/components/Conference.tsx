3eb72f33c48b0b3bd36d2f2b7863d01d6d93b0a3
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2026-03-14 12:10:15 +0100

    multistrim

--- react-client/src/components/Conference.tsx
+++ react-client/src/components/Conference.tsx
@@ -101,7 +101,10 @@ export function InConference({ name, leaveConference }: iInConference) {
       newContact.name,
     );
     setStreams((oldStreams) => {
-      const alreadyExists = oldStreams.some((s) => s.id === newStream.id);
+      console.log(oldStreams);
+      const alreadyExists = oldStreams.some(
+        (s) => s.ownerName === newContact.name,
+      );
       if (alreadyExists) return oldStreams;
 
       return [...oldStreams, newStream];
